d65e6873c4bb5c2e3c1ec97f24dce9d0982bae8d
Phil Selker <26040772+pselker2@users.noreply.github.com>
2020-02-08 17:25:55 -0500

    Initial upload

---
+++ config.py
@@ -0,0 +1,10 @@
+
+# CodeFest 2020
+
+# This file contains the:
+# ServiceNow developer instance info, username and password
+
+SNOW_URL = 'https://dev123455.service-now.com/api/now'
+SNOW_ADMIN = 'admin'
+SNOW_PASS = 'password'
+SNOW_TABLE = 'x_123456_ios_xe_al_iosxe_events'
